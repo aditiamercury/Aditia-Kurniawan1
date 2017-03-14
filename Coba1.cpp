8b929a890a28f79b4e579c3b63009376a9a830e3
aditiamercury <aditialennon@gmail.com>
2017-03-14 20:51:05 +0700

    Add files via upload

---
+++ Coba1.cpp
@@ -0,0 +1,19 @@
+\\Aditia Kurniawan 16.111.155 TIF-RM 16C
+
+#include <iostream>
+using namespace std;
+
+int main () {
+	int a, b;
+	int c;
+	float f;
+
+	a = 10;
+	b = 20;
+	c = a + b;
+
+cout << c << endl;
+f = 70.0/3.0;
+cout << f << endl;
+return 0;
+}
